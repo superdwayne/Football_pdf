71d2b0ce8cd49b063ff5f3883175052edd9a10c2
Dwayne Paisley-Marshall <dwayne@Mac.home>
2025-11-19 16:44:40 +0100

    Add Next.js config to include Chromium binary in bundle

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   // Server Actions are enabled by default in Next.js 16+
   serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium-min"],
+  experimental: {
+    serverComponentsExternalPackages: ["@sparticuz/chromium-min"],
+  },
+  outputFileTracingIncludes: {
+    "/api/pdf/**": ["./node_modules/@sparticuz/chromium-min/**"],
+    "/api/pdf/from-airtable/**": ["./node_modules/@sparticuz/chromium-min/**"],
+  },
 }
 
 module.exports = nextConfig
